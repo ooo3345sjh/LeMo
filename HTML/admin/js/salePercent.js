d9ce075a022e4e4adaeceb6b9814c65415af289b
Yiwonjeong <near_and_dear_studio@naver.com>
2023-03-05 23:14:28 +0900

    23/03/05 이원정/HTML/관리자 화면 구현 수정

--- HTML/admin/js/salePercent.js
+++ HTML/admin/js/salePercent.js
@@ -1,4 +1,5 @@
 // 할인율 적용 계산
+/*
 function insertSalePercent(){
     let OriPrice = $(this).parent().prev().children().val();
 
@@ -12,7 +13,7 @@ function insertSalePercent(){
 
     $(this).parent().next().text(SalePrice);
 }
-
+*/
 /*
 $(document).ready( function() {
     $("#insertSalePercent").change(function () { 
@@ -32,3 +33,46 @@ $(document).ready( function() {
     });
 });
 */
+// 성수기 주중 
+$(document).ready(function(){
+    $('#insertSalePercent').change(function(){
+        let OriPrice = $('#insertPrice').val();
+        let percent = $(this).val();
+
+        let SalePrice = OriPrice - (OriPrice * (percent/100));
+        $('#priceResult').text(SalePrice);
+    });
+});
+
+// 성수기 주말
+$(document).ready(function(){
+    $('#insertSalePercent2').change(function(){
+        let OriPrice = $('#insertPrice2').val();
+        let percent = $(this).val();
+
+        let SalePrice = OriPrice - (OriPrice * (percent/100));
+        $('#priceResult2').text(SalePrice);
+    });
+});
+
+// 비성수기 주중 
+$(document).ready(function(){
+    $('#insertSalePercent3').change(function(){
+        let OriPrice = $('#insertPrice3').val();
+        let percent = $(this).val();
+
+        let SalePrice = OriPrice - (OriPrice * (percent/100));
+        $('#priceResult3').text(SalePrice);
+    });
+});
+
+// 비성수기 주말
+$(document).ready(function(){
+    $('#insertSalePercent4').change(function(){
+        let OriPrice = $('#insertPrice4').val();
+        let percent = $(this).val();
+
+        let SalePrice = OriPrice - (OriPrice * (percent/100));
+        $('#priceResult4').text(SalePrice);
+    });
+});
