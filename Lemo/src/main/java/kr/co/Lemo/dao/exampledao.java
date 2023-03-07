454f5b1086235bda6706854c85145bfd8004a34e
lazca <lazca2080@gmail.com>
2023-03-07 16:41:45 +0900

    2023/03/07

---
+++ Lemo/src/main/java/kr/co/Lemo/dao/exampledao.java
@@ -0,0 +1,7 @@
+package kr.co.Lemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class exampledao {
+}
---
+++ Lemo/src/main/java/kr/co/Lemo/entity/exampleEntity.java
@@ -0,0 +1,4 @@
+package kr.co.Lemo.entity;
+
+public class exampleEntity {
+}
