043b90d452e5c83153e43fe296cc9bc323708629
lazca <lazca2080@gmail.com>
2023-03-07 17:20:55 +0900

    2023/03/07

---
+++ Lemo/src/main/java/kr/co/Lemo/controller/productController.java
@@ -0,0 +1,19 @@
+package kr.co.Lemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @since 2023/03/07
+ * @author 박종협
+ * @apiNote 상품 controller
+ */
+
+@Controller
+public class productController {
+
+    @GetMapping("layouts/layout")
+    public String test(){
+        return "layouts/layout";
+    }
+}
