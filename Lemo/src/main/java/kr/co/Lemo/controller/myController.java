ed47ddf7ef7e348707c086a6d31d1116286fca81
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-03-07 18:23:04 +0900

    fixed untracked files

---
+++ Lemo/src/main/java/kr/co/Lemo/controller/myController.java
@@ -0,0 +1,10 @@
+package kr.co.Lemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class myController {
+
+    
+}
