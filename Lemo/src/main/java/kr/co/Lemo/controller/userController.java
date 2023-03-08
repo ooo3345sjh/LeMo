57ae38cdba5ddafb17ba7d9d065a4f607dcf34a4
ooo3345sjh <ooo3345sjh@gamil.com>
2023-03-08 19:23:20 +0900

    2023/03/08 서정현/ 회원 >  약관동의 기능구현중

--- Lemo/src/main/java/kr/co/Lemo/controller/userController.java
+++ Lemo/src/main/java/kr/co/Lemo/controller/userController.java
@@ -2,6 +2,7 @@ package kr.co.Lemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,10 +17,10 @@ public class userController {
     @GetMapping("join")
     public String join() {
         return "user/join";
-
     }
     @GetMapping("terms")
     public String terms() {
+
         return "user/terms";
     }
 
