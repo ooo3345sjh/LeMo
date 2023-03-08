6344cb29557a5e3bf6a703b44e449975c982a6b5
lazca <lazca2080@gmail.com>
2023-03-08 16:49:49 +0900

    2023/03/08

---
+++ Lemo/src/main/java/kr/co/Lemo/domain/diarySpotVO.java
@@ -0,0 +1,27 @@
+package kr.co.Lemo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @since 2023/03/08
+ * @author 박종협
+ * @apiNote my/diary SpotVO
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class diarySpotVO {
+    private int spot_no;
+    private int arti_no;
+    private String spot_longtitude;
+    private String spot_lattitude;
+    private int spot_xy;
+    private String spot_content;
+    private String spot_thumb;
+    private String province_name;
+    private String spot_addr;
+}
---
+++ Lemo/src/main/java/kr/co/Lemo/service/myService.java
@@ -0,0 +1,4 @@
+package kr.co.Lemo.service;
+
+public class myService {
+}
