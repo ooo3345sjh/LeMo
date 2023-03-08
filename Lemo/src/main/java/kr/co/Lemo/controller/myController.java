19529c6cfde4415dbe3b87adf513fb368281769f
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-03-08 14:51:14 +0900

    2023/03/08 황원진 마이페이지 HTML 변환

--- Lemo/src/main/java/kr/co/Lemo/controller/myController.java
+++ Lemo/src/main/java/kr/co/Lemo/controller/myController.java
@@ -2,6 +2,7 @@ package kr.co.Lemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @since 2023/03/07
@@ -10,20 +11,46 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 
 @Controller
+@RequestMapping("my/")
 public class myController {
 
-    @GetMapping("my/coupon")
+    @GetMapping("coupon")
     public String coupon(){
         return "my/coupon";
     }
 
-    @GetMapping("my/info")
+    @GetMapping("info")
     public String info(){
         return "my/info";
     }
 
-    @GetMapping("my/pick")
+    @GetMapping("pick")
     public String pick(){
         return "my/pick";
     }
+
+    @GetMapping("point")
+    public String point() {
+        return "my/point";
+    }
+
+    // @since 2023/03/08
+    @GetMapping("reserveration")
+    public String reserveration() {return "my/reserveration";}
+
+    // @since 2023/03/08
+    @GetMapping("view")
+    public String view() {return "my/view";}
+
+    @GetMapping("review/list")
+    public  String reviewList() {return "my/review/list";}
+
+    @GetMapping("review/modify")
+    public String reviewModify() {return "my/review/modify";}
+
+    @GetMapping("review/view")
+    public String reviewView() {return "my/review/view";}
+
+    @GetMapping("review/write")
+    public String reviewWrite() {return  "my/review/write";}
 }
