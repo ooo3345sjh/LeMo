832d00e7f978442de251170c20be218d27116262
lazca <lazca2080@gmail.com>
2023-03-09 19:06:08 +0900

    2023/03/09 박종협 my/diary/write 기능 구현 중

--- Lemo/src/main/java/kr/co/Lemo/controller/MyController.java
+++ Lemo/src/main/java/kr/co/Lemo/controller/MyController.java
@@ -1,6 +1,10 @@
 package kr.co.Lemo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,36 +15,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 
 @Controller
+@PropertySource(value = "classpath:title.properties", encoding = "UTF-8")
+@RequiredArgsConstructor
 @RequestMapping("my/")
 public class MyController {
 
+    private final Environment environment;
+
+    private String myGroup = "title.my";
+    private String diaryGroup = "title.diary";
+
     @GetMapping("coupon")
-    public String coupon(){
+    public String coupon(Model m){
+        m.addAttribute("title", environment.getProperty(myGroup));
         return "my/coupon";
     }
 
     @GetMapping("info")
-    public String info(){
+    public String info(Model m){
+        m.addAttribute("title", environment.getProperty(myGroup));
         return "my/info";
     }
 
     @GetMapping("pick")
-    public String pick(){
+    public String pick(Model m){
+        m.addAttribute("title", environment.getProperty(myGroup));
         return "my/pick";
     }
 
     @GetMapping("point")
-    public String point() {
+    public String point(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
         return "my/point";
     }
 
     // @since 2023/03/08
-    @GetMapping("reserveration")
-    public String reserveration() {return "my/reserveration";}
+    @GetMapping("reservation")
+    public String reservation(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return "my/reservation";
+    }
 
     // @since 2023/03/08
     @GetMapping("view")
-    public String view() {return "my/view";}
+    public String view(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return "my/view";
+    }
 
 
     /*
@@ -48,32 +69,53 @@ public class MyController {
     *   @apiNote : 나의 리뷰
     * */
     @GetMapping("review/list")
-    public  String reviewList() {return "my/review/list";}
+    public  String reviewList(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return "my/review/list";
+    }
 
     @GetMapping("review/modify")
-    public String reviewModify() {return "my/review/modify";}
+    public String reviewModify(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return "my/review/modify";
+    }
 
     @GetMapping("review/view")
-    public String reviewView() {return "my/review/view";}
+    public String reviewView(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return "my/review/view";
+    }
 
     @GetMapping("review/write")
-    public String reviewWrite() {return  "my/review/write";}
+    public String reviewWrite(Model m) {
+        m.addAttribute("title", environment.getProperty(myGroup));
+        return  "my/review/write";
+    }
 
     /*
      *   @since : 2023/03/08
      *   @apiNote : 나의 여행일기
      * */
     @GetMapping("diary/list")
-    public String diary_list() {return "my/diary/list";}
+    public String diary_list(Model m) {
+        m.addAttribute("title", environment.getProperty(diaryGroup));
+        return "my/diary/list";
+    }
 
     /**
      * @since 2023/03/08
      * @author 박종협
      */
     @GetMapping("diary/write")
-    public String diary_write() {return "my/diary/write";}
+    public String diary_write(Model m) {
+        m.addAttribute("title", environment.getProperty(diaryGroup));
+        return "my/diary/write";
+    }
 
     @GetMapping("diary/modify")
-    public String diary_modify() {return "my/diary/modify";}
+    public String diary_modify(Model m) {
+        m.addAttribute("title", environment.getProperty(diaryGroup));
+        return "my/diary/modify";
+    }
 
 }
