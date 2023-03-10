5d168e041627aa27ec6613c3f799ea935e96e63c
Yiwonjeong <near_and_dear_studio@naver.com>
2023-03-10 14:10:16 +0900

    23/03/10 이원정/관리자 회원 목록 기능 구현

--- Lemo/src/main/java/kr/co/Lemo/dao/AdminDAO.java
+++ Lemo/src/main/java/kr/co/Lemo/dao/AdminDAO.java
@@ -3,6 +3,7 @@ package kr.co.Lemo.dao;
 import kr.co.Lemo.domain.UserVO;
 import kr.co.Lemo.utils.SearchCondition;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +13,8 @@ import java.util.List;
 public interface AdminDAO {
     public List<UserVO> selectUser(SearchCondition sc);
     public int countUser(SearchCondition sc);
+    public int updateMemo(@Param("memo") String memo, @Param("user_id") String user_id);
+    public int updateIsLocked(@Param("user_id") String user_id);
+    public int updateIsEnabled(@Param("user_id") String user_id);
+
 }
