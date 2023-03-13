e399c45e808f6a27c589c0b47f0328fc70b0f0a8
ooo3345sjh <ooo3345sjh@gamil.com>
2023-03-13 19:29:52 +0900

    2023/03/13 서정현/ 업데이트

--- Lemo/src/main/java/kr/co/Lemo/utils/SearchCondition_v2.java
+++ Lemo/src/main/java/kr/co/Lemo/utils/SearchCondition_v2.java
@@ -16,7 +16,6 @@ public class SearchCondition_v2 {
     private Integer page = 1;
     private Integer pageSize = 10;
     private Integer no = 0;
-    private Map<String, String> map;
 
     public String getQueryString(Integer page){
         // ?page=1&pageSize=10&option="T"&keyword="title"
@@ -28,19 +27,8 @@ public class SearchCondition_v2 {
         UriComponentsBuilder builder = UriComponentsBuilder.newInstance()
                 .queryParam("page", page);
 
-
         if (no != null && no != 0)
             builder.queryParam("no", no);
-
-        for(String key : map.keySet()){
-            String value = map.get(key);
-
-            if(!"page".equals(key) && !"no".equals(key)){
-
-                if(value != null && !value.isBlank())
-                    builder.queryParam(key, value);
-            }
-        }
 
         return builder.toUriString();
     }
