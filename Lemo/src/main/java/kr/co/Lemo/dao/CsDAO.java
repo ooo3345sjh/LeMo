a9fd96b36c905516bbc301cfcf042b6824a9b800
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-03-14 13:40:50 +0900

    2023/03/14 황원진/admin/cs/notice/view 기능구현, /modify 기능구현중

--- Lemo/src/main/java/kr/co/Lemo/dao/CsDAO.java
+++ Lemo/src/main/java/kr/co/Lemo/dao/CsDAO.java
@@ -46,6 +46,7 @@ public interface CsDAO {
     //@since 2023/03/10
     public int insertArticleQna(CsVO vo);
     /** update **/
+    //@since 2023/03/14
     public int updateQnaArticle(@Param("cs_reply") String cs_reply, @Param("cs_no") int cs_no);
 
     /** delete **/
