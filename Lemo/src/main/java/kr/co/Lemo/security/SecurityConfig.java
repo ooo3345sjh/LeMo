b7dd98b3a33b43b1980d16fd139cbe276b3bc940
ooo3345sjh <ooo3345sjh@gamil.com>
2023-04-13 19:30:35 +0900

    2023/04/13 서정현 업데이트

--- Lemo/src/main/java/kr/co/Lemo/security/SecurityConfig.java
+++ Lemo/src/main/java/kr/co/Lemo/security/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
