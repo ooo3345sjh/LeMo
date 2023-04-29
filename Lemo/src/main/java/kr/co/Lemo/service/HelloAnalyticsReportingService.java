cff6f410a447f888f23da4f8b42f1c7fc91c0992
ooo3345sjh <ooo3345sjh@gmail.com>
2023-04-29 21:49:16 +0900

    2023/04/29 서정현

---
+++ Lemo/src/main/java/kr/co/Lemo/service/HelloAnalyticsReportingService.java
@@ -0,0 +1,81 @@
+package kr.co.Lemo.service;
+
+import com.google.analytics.data.v1beta.*;
+import com.google.analytics.data.v1beta.DateRange;
+import com.google.analytics.data.v1beta.Dimension;
+import com.google.analytics.data.v1beta.Metric;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+
+public class HelloAnalyticsReportingService {
+  @Value("${spring.servlet.multipart.location}")
+  private String uploadPath;
+
+  public static void main(String... args) throws Exception {
+    /**
+     * TODO(developer): Replace this variable with your Google Analytics 4 property ID before
+     * running the sample.
+     */
+    String propertyId = "373040723";
+
+
+    /**
+     * TODO(developer): Replace this variable with a valid path to the credentials.json file for
+     * your service account downloaded from the Cloud Console.
+     */
+    String credentialsJsonPath = "C:/Users/ooo33.DESKTOP-56U45AS/Desktop/workspace/LeMo/Lemo/file/credentials_y.json";
+    File file = new File(credentialsJsonPath);
+    System.out.println(file.exists());
+    sampleRunReport(propertyId, credentialsJsonPath);
+  }
+
+  // This is an example snippet that calls the Google Analytics Data API and runs a simple report
+  // on the provided GA4 property id.
+  static void sampleRunReport(String propertyId, String credentialsJsonPath) throws Exception {
+    // [START analyticsdata_json_credentials_initialize]
+    // Explicitly use service account credentials by specifying
+    // the private key file.
+    GoogleCredentials credentials =
+            GoogleCredentials.fromStream(new FileInputStream(credentialsJsonPath));
+
+    BetaAnalyticsDataSettings betaAnalyticsDataSettings =
+            BetaAnalyticsDataSettings.newBuilder()
+                    .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                    .build();
+
+    try (BetaAnalyticsDataClient analyticsData =
+                 BetaAnalyticsDataClient.create(betaAnalyticsDataSettings)) {
+      // [END analyticsdata_json_credentials_initialize]
+
+      // [START analyticsdata_json_credentials_run_report]
+      RunReportRequest request =
+              RunReportRequest.newBuilder()
+                      .setProperty("properties/" + propertyId)
+                      .addDimensions(Dimension.newBuilder().setName("city"))
+                      .addMetrics(Metric.newBuilder().setName("activeUsers"))
+                      .addDateRanges(DateRange.newBuilder().setStartDate("2023-04-28").setEndDate("2023-04-29"))
+                      .build();
+
+      // Make the request.
+      RunReportResponse response = analyticsData.runReport(request);
+      // [END analyticsdata_json_credentials_run_report]
+
+      // [START analyticsdata_json_credentials_print_report]
+      System.out.println("Report result:");
+      System.out.println(response.getRowCount());
+      // Iterate through every row of the API response.
+      for (Row row : response.getRowsList()) {
+        System.out.printf(
+                "%s, %s%n", row.getDimensionValues(0).getValue(), row.getMetricValues(0).getValue());
+      }
+      // [END analyticsdata_json_credentials_print_report]
+    }
+  }
+    
+}
+
