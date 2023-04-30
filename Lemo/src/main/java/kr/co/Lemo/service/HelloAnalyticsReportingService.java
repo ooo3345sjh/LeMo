3f8f74b8a692bc8548bbb0eff05b6cd9288b1692
ooo3345sjh <ooo3345sjh@gmail.com>
2023-04-30 22:54:12 +0900

    2023/04/30

--- Lemo/src/main/java/kr/co/Lemo/service/HelloAnalyticsReportingService.java
+++ Lemo/src/main/java/kr/co/Lemo/service/HelloAnalyticsReportingService.java
@@ -21,20 +21,14 @@ public class HelloAnalyticsReportingService {
   @Value("${spring.servlet.multipart.location}")
   private String uploadPath;
 
-  public List<AnalyticsVO> getData(String startDate, String endDate) throws Exception {
-    // TODO(developer): Replace this variable with your Google Analytics 4 property ID before
-    String propertyId = "373040723";
+  @Value("${analytics.propertyId}")
+  private String propertyId;
 
-    // TODO(developer): Replace this variable with a valid path to the credentials.json file for
+  private Integer getData(String startDate, String endDate) throws Exception {
+    int totalUsers = 0;
+
     // credentialsJsonPath: 인증 정보가 포함된 Service Account Key 파일의 경로
     String credentialsJsonPath = uploadPath + "/credentials.json";
-
-    // 파일 유무 확인 (true, false)
-    File file = new File(credentialsJsonPath);
-    System.out.println(file.exists());
-    // lemoRunReport(propertyId, credentialsJsonPath);
-
-    List<AnalyticsVO> reportData = new ArrayList<>();
 
     // GoogleCredentials: GCP(Google Cloud Platform)에서 제공하는 서비스에 인증된 사용자의 자격 증명을 가져오기 위함
     // fromStream() 메서드: FileInputStream 객체를 인수로 받아 인증 정보를 생성 (Service Account Key 파일을 사용하여 인증 정보 생성)
@@ -44,12 +38,6 @@ public class HelloAnalyticsReportingService {
     BetaAnalyticsDataSettings betaAnalyticsDataSettings = BetaAnalyticsDataSettings.newBuilder()
                                                           .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
                                                           .build();
-
-    // 오늘 날짜 설정
-    LocalDate today = LocalDate.now();
-    String todayStr = today.toString();
-
-    log.debug("todayStr : " + todayStr);
 
     try (BetaAnalyticsDataClient analyticsData =
                  BetaAnalyticsDataClient.create(betaAnalyticsDataSettings)) {
@@ -61,7 +49,7 @@ public class HelloAnalyticsReportingService {
       RunReportRequest request =
               RunReportRequest.newBuilder()
                       .setProperty("properties/" + propertyId)
-                      .addDimensions(Dimension.newBuilder().setName("country"))      // addDimensions() 메서드는 보고서의 차원(Dimension)을 추가 (ex. 'city' 차원 추가)
+                      .addDimensions(Dimension.newBuilder().setName("date"))      // addDimensions() 메서드는 보고서의 차원(Dimension)을 추가 (ex. 'city' 차원 추가)
                       .addMetrics(Metric.newBuilder().setName("activeUsers"))     // addMetrics() 메서드는 보고서의 측정 항목(Metric, 수집된 데이터에서 계산한 측정값)을 추가, (ex. activeUsers)
                       .addDateRanges(DateRange.newBuilder()
                               .setStartDate(startDate)
@@ -69,28 +57,21 @@ public class HelloAnalyticsReportingService {
                       )  // addDateRanges() 메서드는 보고서에 포함할 날짜 범위(DateRange)를 추가
                       .build();
 
-      // Make the request.
       RunReportResponse response = analyticsData.runReport(request);
-      // [END analyticsdata_json_credentials_run_report]
+      log.info(response + "");
+      for (Row row : response.getRowsList())
+        totalUsers += Integer.parseInt(row.getMetricValues(0).getValue());
 
-      // [START analyticsdata_json_credentials_print_report]
-      System.out.println("Report result:");
-      System.out.println(response.getRowCount());
-      // Iterate through every row of the API response.
-      for (Row row : response.getRowsList()) {
-        System.out.printf(
-                "%s, %s%n", row.getDimensionValues(0).getValue(), row.getMetricValues(0).getValue());
-         AnalyticsVO vo = new AnalyticsVO();
-        vo.setCountry(row.getDimensionValues(0).getValue());
-        vo.setActiveUsers(Integer.parseInt(row.getMetricValues(0).getValue()));
-
-        reportData.add(vo);
-      }
-      // [END analyticsdata_json_credentials_print_report]
     }
-    return reportData;
+    return totalUsers;
+  }
 
+  public Integer getTodayActiveUser() throws Exception {
+    return getData("today", "today");
+  }
 
+  public Integer getActiveUser(String startDate, String endDate) throws Exception {
+    return getData(startDate, endDate);
   }
 
 }
